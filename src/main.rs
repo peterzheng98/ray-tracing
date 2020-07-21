cbcfcd058b4352ab7b0ef71e86bbf4907f5e88b0
Wenxin Zheng <peterzheng98@sjtu.edu.cn>
2020-07-21 10:17:07 +0800

    clock finished

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn main() {
     let radius: f32 = 135.0;
     let left_point = circle_x - radius;
     let right_point = circle_x + radius;
-    let step: f32 = 0.01;
+    let step: f32 = 0.001;
     let mut start_point: f32 = left_point;
     while start_point <= right_point{
         let y0_minus = ((radius * radius - ((start_point - circle_x) * (start_point - circle_x))) as f32).sqrt();
@@ -31,7 +31,36 @@ fn main() {
         *imgbuf.get_pixel_mut(start_point as u32, y1 as u32) = image::Rgb([255 as u8, 255 as u8, 255 as u8]);
         start_point = start_point + step;
     }
-    
+
+    let mut hour_radius: f32 = 100.0;
+    let mut ang = 0.33333 * std::f32::consts::PI;
+    let mut rad = 0.0;
+    while rad <= hour_radius{
+        let hour_x = circle_x + rad * ang.cos();
+        let hour_y = circle_y - rad * ang.sin();
+        *imgbuf.get_pixel_mut(hour_x as u32, hour_y as u32) = image::Rgb([255 as u8, 255 as u8, 255 as u8]);
+        rad = rad + step;
+    }
+
+    hour_radius = 75.0;
+    ang = 0.7250 * std::f32::consts::PI;
+    rad = 0.0;
+    while rad <= hour_radius{
+        let hour_x = circle_x + rad * ang.cos();
+        let hour_y = circle_y - rad * ang.sin();
+        *imgbuf.get_pixel_mut(hour_x as u32, hour_y as u32) = image::Rgb([255 as u8, 255 as u8, 255 as u8]);
+        rad = rad + step;
+    }
+
+    hour_radius = 50.0;
+    ang = 1.561 * std::f32::consts::PI;
+    rad = 0.0;
+    while rad <= hour_radius{
+        let hour_x = circle_x + rad * ang.cos();
+        let hour_y = circle_y - rad * ang.sin();
+        *imgbuf.get_pixel_mut(hour_x as u32, hour_y as u32) = image::Rgb([255 as u8, 255 as u8, 255 as u8]);
+        rad = rad + step;
+    }
 
 
     match imgbuf.save("_output_/output.png"){
